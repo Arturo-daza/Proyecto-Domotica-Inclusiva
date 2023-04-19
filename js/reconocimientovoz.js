f4aec8d6f1d4676629ca58f5d238f34281d86162
Arturo-daza <davisdaza70@gmail.com>
2023-04-19 13:24:36 -0500

    Comandos basicos de voz

--- js/reconocimientovoz.js
+++ js/reconocimientovoz.js
@@ -7,28 +7,99 @@ function activarReconocimientoDeVoz() {
     reconocimientoDeVoz.lang = "es-ES";
     reconocimientoDeVoz.interimResults = true;
     reconocimientoDeVoz.maxSilence = TIEMPO_MAXIMO_SILENCIO;
-    
     const texto = document.getElementById("mostrar");
 
     reconocimientoDeVoz.onresult = function (event) {
-    const resultado = event.results[0][0].transcript;
-    texto.textContent = resultado;
+        const resultado = event.results[0][0].transcript;
+        texto.textContent = resultado;
+        procesarResultado(resultado);
+
     };
 
     reconocimientoDeVoz.onend = function () {
-    document.querySelector("button").textContent = "Activar micrófono";
-    document.querySelector("button").onclick = function() {activarReconocimientoDeVoz()};
+        document.querySelector("button").textContent = "Activar micrófono";
+        document.querySelector("button").onclick = function () { activarReconocimientoDeVoz() };
     }
 
     reconocimientoDeVoz.start();
 
     document.querySelector("button").textContent = "Desactivar micrófono";
-    document.querySelector("button").onclick = function() {desactivarReconocimientoDeVoz()};
+    document.querySelector("button").onclick = function () { desactivarReconocimientoDeVoz() };
 }
 
 function desactivarReconocimientoDeVoz() {
     reconocimientoDeVoz.stop();
     document.querySelector("button").textContent = "Activar micrófono";
-    document.querySelector("button").onclick = function() {activarReconocimientoDeVoz()};
+    document.querySelector("button").onclick = function () { activarReconocimientoDeVoz() };
     clearTimeout(apagarDespuesDe);
+}
+
+function procesarResultado(resultado) {
+    // Minuscula  para analizar la cadena de caracteres
+    resultado = resultado.toLowerCase();
+    const numero = extraerNumero(resultado);
+    const texto_resultado = document.getElementById("resulado");
+    var respuesta = ""; 
+
+    if (resultado.includes('abrir')) {
+        if (resultado.includes('puerta')) {
+            if (resultado.includes('habitación')) {
+                if (numero) {
+                    if (numero > 3) {
+                        respuesta = `No existe la habitacion ${numero}.`;
+                        texto_resultado.textContent= respuesta;
+                    } else {
+                        respuesta =`Se va abrir la puerta de la habitacion ${numero}.`;
+                        texto_resultado.textContent= respuesta;
+                    }
+                } else {
+                    resultado = 'No se especifica la habitación'; 
+                    texto_resultado.textContent= resultado;
+                }
+            } else if (resultado.includes('principal')) {
+                resultado = 'No se especifica la habitación';
+                texto_resultado.textContent= resultado;
+                console.log('principal');
+            }
+        } else if (resultado.includes('ventana')) {
+            if (resultado.includes('habitación')) {
+                if (numero) {
+                    if (numero > 3) {
+                        respuesta = `No existe la habitacion ${numero}.`;
+                        texto_resultado.textContent= respuesta;
+                    } else {
+                        respuesta =`Se va abrir la ventana de la habitacion ${numero}.`;
+                        texto_resultado.textContent= respuesta;
+                    }
+                } else {
+                    resultado = 'No se especifica la habitación'; 
+                    texto_resultado.textContent= resultado;
+                }
+            }
+            console.log(`Se va a apagar la luz con el número ${numero}.`);
+        } else {
+            console.log('No se reconoce la acción a realizar.');
+        }
+    } else {
+        console.log('La resultado de texto no contiene la palabra "luz".');
+    }
+    if(respuesta == ""){
+        texto_resultado.textContent= respuesta;
+    }
+
+}
+
+function extraerNumero(resultado) {
+    const matches = resultado.match(/\d+/); // Busca uno o más dígitos en la cadena
+    var regex = /\b(cuatro|dos)\b/gi;
+    if (matches) {
+        return matches[0]; // Retorna el primer número encontrado
+    // Los numeros 2 y 4 el conocimiento de voz lo escribe en texto
+    } else if (resultado.includes('dos')) {
+        return 2;
+    }else if (resultado.includes('cuatro')){
+        return 4; 
+    }else{
+        return null; // Retorna null si no se encontraron números en la cadena
+    }
 }
