a0176c29bc29b20a39a365c4422bdb59658dd2e7
Wolfam Diaz Galeano <wolfamc.diazg@konradlorenz.edu.co>
2023-05-14 07:35:19 -0500

    ComunicacionSerial

--- funcionArduino.py
+++ funcionArduino.py
@@ -1,4 +1,56 @@
+import serial
+
+#ser = serial.Serial('COM3', 9600,timeout=1)
+#ser.write(b'a')
 def enviarArduino(ubicaionesPuerta, ubicacionesLuz, ubicacionesVentana):
     print(ubicacionesLuz)
-    print(ubicacionesVentana)
-    print(ubicaionesPuerta)
+
+
+     
+    if ubicacionesLuz['principales']==True:
+            print("PuertaPrincipal LuzEncendida")
+    if ubicacionesLuz['principales']==False:
+            print("PuertaPrincipal LuzApagada")   
+    if ubicacionesLuz['estudio']==True:
+            print("estudio LuzEncendida")
+    if ubicacionesLuz['estudio']==False:
+            print("estudio LuzApagada")
+    if ubicacionesLuz['habitación 3']==True:
+            print("habitacion 3 Luz Encendida")
+    if ubicacionesLuz['habitación 3']==False:
+            print("habitacion 3 LuzApagada")
+
+
+    if ubicaionesPuerta['principales']==True:
+            print("PuertaPrincipal abierta")
+    if ubicaionesPuerta['principales']==False:
+            print("PuertaPrincipal cerrada")   
+    if ubicaionesPuerta['estudio']==True:
+            print("Puerta estudio abierta")
+    if ubicaionesPuerta['estudio']==False:
+            print("Puerta estudio cerrada")
+
+    if ubicaionesPuerta['habitación 3']==True:
+            print("Puerta habitación 3 abierta")
+    if ubicaionesPuerta['habitación 3']==False:
+            print("habitacion 3 LuzApagada")
+    
+
+    if ubicacionesVentana['principales']==True:
+            print("Ventana Principal Abierta")
+    if ubicacionesVentana['principales']==False:
+            print("Ventana Principal Cerrada")   
+    if ubicacionesVentana['estudio']==True:
+            print("Ventana estudio abierta")
+    if ubicacionesVentana['estudio']==False:
+            print("Ventana estudio cerrada")
+
+    if ubicacionesVentana['habitación 3']==True:
+            print("ventana habitacion 3 abierta")
+    if ubicacionesVentana['habitación 3']==False:
+            print("ventana habitacion 3 cerrada")
+        
+        
+
+
+        
