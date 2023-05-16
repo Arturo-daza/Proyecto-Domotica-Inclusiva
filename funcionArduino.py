c928a87bc84563bc45288bd472183855ac1adf49
Arturo-daza <davisdaza70@gmail.com>
2023-05-16 10:48:24 -0500

    funcionArduino

--- funcionArduino.py
+++ funcionArduino.py
@@ -1,28 +1,45 @@
 import serial
 
-def enviarArduino(ubicaionesPuerta, ubicacionesLuz, ubicacionesVentana):
-    print(ubicacionesLuz)
-        
-   
 
-     
+
+def enviarArduino(ubicaionesPuerta, ubicacionesLuz, ubicacionesVentana):
+    print(ubicacionesLuz)    
+    arduino=serial.Serial('COM5',9600)      
     if ubicacionesLuz['principales']==True:
-            arduino=serial.Serial('COM5',9600) 
+            arduino.open()
             print("Principal LuzEncendida")
             for i in range(1, 50):
              arduino.write(b'q')
+             arduino.close()  
            
     elif ubicacionesLuz['principales']==False:
             arduino.open()
             print("Principal Apagada")
             for i in range(1, 50):
              arduino.write(b'w')
-            arduino.close()
-           
-                   
-            
+            arduino.close()    
     else:
         print("AyudaDios")
+
+
+# arduino=serial.Serial('COM5',9600) 
+# def enviarArduino(ubicaionesPuerta, ubicacionesLuz, ubicacionesVentana):
+#     print(ubicacionesLuz)    
+#     if ubicacionesLuz['principales']==True:
+#             arduino.open()
+#             print("Principal LuzEncendida")
+#             for i in range(1, 50):
+#              arduino.write(b'q')
+#              arduino.close()  
+           
+#     elif ubicacionesLuz['principales']==False:
+#             arduino.open()
+#             print("Principal Apagada")
+#             for i in range(1, 50):
+#              arduino.write(b'w')
+#             arduino.close()    
+#     else:
+#         print("AyudaDios")
 
    
     
