a8fd0afa4f378983ec4800b4c0e9dd66941c40bc
Arturo-daza <davisdaza70@gmail.com>
2023-05-16 10:51:46 -0500

    funcionArduino

--- funcionArduino.py
+++ funcionArduino.py
@@ -22,6 +22,28 @@ def enviarArduino(ubicaionesPuerta, ubicacionesLuz, ubicacionesVentana):
         print("AyudaDios")
 
 
+# def enviarArduino(ubicacionesPuerta, ubicacionesLuz, ubicacionesVentana):
+#     print(ubicacionesLuz)
+#     arduino = serial.Serial('COM5', 9600)
+    
+#     if ubicacionesLuz['principales'] == True:
+#         print("Principal Luz Encendida")
+#         for i in range(1, 50):
+#             arduino.open()
+#             arduino.write(b'q')
+#             arduino.close()
+           
+#     elif ubicacionesLuz['principales'] == False:
+#         print("Principal Apagada")
+#         for i in range(1, 50):
+#             arduino.open()
+#             arduino.write(b'w')
+#             arduino.close()
+    
+#     else:
+#         print("AyudaDios")
+
+
 # arduino=serial.Serial('COM5',9600) 
 # def enviarArduino(ubicaionesPuerta, ubicacionesLuz, ubicacionesVentana):
 #     print(ubicacionesLuz)    
