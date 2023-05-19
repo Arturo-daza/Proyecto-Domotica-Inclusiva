de00b9f4e19b599f3f8e755a33c579970c8de50e
Arturo-daza <davisdaza70@gmail.com>
2023-05-19 11:23:10 -0500

    funcionArduino

--- funcionArduino.py
+++ funcionArduino.py
@@ -1,69 +1,18 @@
 import serial
 
+import serial
+import time
+
 
 
 def enviarArduino(ubicaionesPuerta, ubicacionesLuz, ubicacionesVentana):
-    print(ubicacionesLuz)    
-    arduino=serial.Serial('COM5',9600)      
+    arduino=serial.Serial('COM3',9600)      
+    time.sleep(2)
     if ubicacionesLuz['principales']==True:
-            arduino.open()
             print("Principal LuzEncendida")
-            for i in range(1, 50):
-             arduino.write(b'q')
-             arduino.close()  
-           
+            arduino.write(b'P')
+            time.sleep(2)
     elif ubicacionesLuz['principales']==False:
-            arduino.open()
             print("Principal Apagada")
-            for i in range(1, 50):
-             arduino.write(b'w')
-            arduino.close()    
-    else:
-        print("AyudaDios")
-
-
-# def enviarArduino(ubicacionesPuerta, ubicacionesLuz, ubicacionesVentana):
-#     print(ubicacionesLuz)
-#     arduino = serial.Serial('COM5', 9600)
-    
-#     if ubicacionesLuz['principales'] == True:
-#         print("Principal Luz Encendida")
-#         for i in range(1, 50):
-#             arduino.open()
-#             arduino.write(b'q')
-#             arduino.close()
-           
-#     elif ubicacionesLuz['principales'] == False:
-#         print("Principal Apagada")
-#         for i in range(1, 50):
-#             arduino.open()
-#             arduino.write(b'w')
-#             arduino.close()
-    
-#     else:
-#         print("AyudaDios")
-
-
-# arduino=serial.Serial('COM5',9600) 
-# def enviarArduino(ubicaionesPuerta, ubicacionesLuz, ubicacionesVentana):
-#     print(ubicacionesLuz)    
-#     if ubicacionesLuz['principales']==True:
-#             arduino.open()
-#             print("Principal LuzEncendida")
-#             for i in range(1, 50):
-#              arduino.write(b'q')
-#              arduino.close()  
-           
-#     elif ubicacionesLuz['principales']==False:
-#             arduino.open()
-#             print("Principal Apagada")
-#             for i in range(1, 50):
-#              arduino.write(b'w')
-#             arduino.close()    
-#     else:
-#         print("AyudaDios")
-
-   
-    
-  
-   
+            arduino.write(b'N')
+            time.sleep(2)
