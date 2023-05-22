d9fc27e1af2e33f86f22ff3a0d6ad847d1224f7e
Arturo-daza <davisdaza70@gmail.com>
2023-05-22 11:42:39 -0500

    Eliminacion de archivos innecesarios

--- conexionArduinoJs/blink.js
+++
@@ -1,8 +0,0 @@
-const five = require('johnny-five');
-
-const board = new five.Board();
-
-board.on('ready', () => {
-  console.log('Board is ready!');
-  // Now you can interact with the board using Johnny-Five APIs
-});
