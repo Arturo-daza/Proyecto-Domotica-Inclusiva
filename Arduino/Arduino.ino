c090cae5902c6d39c1daee2ec5b608016e04fa47
Arturo-daza <davisdaza70@gmail.com>
2023-05-22 11:21:00 -0500

    FuncionArduino V1 finalizada

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,135 +1,151 @@
-// Declara el número del pin donde está conectado el LED
 #include <Servo.h>
-Servo myservo1;
-Servo myservo2;
-int luzHab1 = 2;
-int luzHab2 = 3;
-int luzHab3 = 4;
-int puertaHab1 = 11;
-int ventanaHab1 = 12;
-int ventanaHab2 = 7;
-int ventanaHab3 = 8;
-int ventiladorHab1 = 9;
-int ventiladorHab2 = 10;
+  Servo myservo1;
+  Servo myservo2;
+  Servo myservo3;
+  int luzHab1 = 2;
+  int luzHab2 = 3;
+  int luzBano = 4;
+  int luzSala= 5; 
+  int ventanaHab1 = 6;
+  int ventanaHab2 = 7;
+  int ventanaSala = 8;
+  int ventanaBano = 12;
+  char option;
 
 
-
-char option;
-
-
-void setup() {
-  // Configura el pin como salida
-  myservo1.attach(5);
-  myservo2.attach(12);
-  pinMode(luzHab1, OUTPUT);
-  pinMode(luzHab2, OUTPUT);
-  pinMode(luzHab3, OUTPUT);
- 
-  
-  pinMode(ventanaHab1, OUTPUT);
-  pinMode(ventanaHab2, OUTPUT);
-  pinMode(ventanaHab3, OUTPUT);
-  pinMode(ventiladorHab1, OUTPUT);
-  pinMode(ventiladorHab2, OUTPUT);
-
-  Serial.begin(9600);
-}
-
-void loop() {
-  
-
- 
-  if (Serial.available() > 0) {
-    option = Serial.read();
-
-    switch (option) {
-      //1
-      case 'q':
-        digitalWrite(luzHab1, HIGH);
-        Serial.println("Habitacion 1 Luz encendida");
-        break;
-      case 'w':
-        digitalWrite(luzHab1, LOW);
-        Serial.println("Habitacion 1 Luz apagada");
-        break;
-      //2
-      case 'e':
-        digitalWrite(luzHab2, HIGH);
-        Serial.println("Habitacion 2 Luz encendida");
-        break;
-      case 'r':
-        digitalWrite(luzHab2, LOW);
-        Serial.println("Habitacion 2 Luz apagada");
-        break;
-      case 't':
-        //3
-        digitalWrite(luzHab3, HIGH);
-        Serial.println("Habitacion 3 Luz encendida");
-        break;
-      case 'y':
-        digitalWrite(luzHab3, LOW);
-        Serial.println("Habitacion 3 Luz apagada");
-        break;
-      //4
-      case 'u':
-        digitalWrite(puertaHab1, HIGH);
-        Serial.println("Baño Social Luz encendida");
-        break;
-      case 'i':
-        digitalWrite(puertaHab1, LOW);
-        Serial.println("Baño Social Luz apagada");
-        break;
-
-      //5
-      case 'o':
-         myservo1.write(90);       // mueve el servo hacia la posición 90 grados
-         delay(300);  
-        Serial.println("Baño Social Luz encendida");
-        break;
-      case 'p':
-        myservo1.write(0);       // mueve el servo hacia la posición 90 grados
-        delay(300);  
-        Serial.println("Baño Social Luz apagada");
-        break;
-     
-        
-      //7
-      case 'd':
-        digitalWrite(ventanaHab1, HIGH);
-        Serial.println("Ventana 1 abierta");
-        break;
-      case 'f':
-        digitalWrite(ventanaHab1, LOW);
-        Serial.println("Ventana 1 Cerrada");
-        break;
-      //8
-      case 'g':
-        digitalWrite(ventanaHab2, HIGH);
-        Serial.println("Ventana 1 abierta");
-        break;
-      case 'h':
-        digitalWrite(ventanaHab2, LOW);
-        Serial.println("Ventana 1 Cerrada");
-        break;
-      //9
-      case 'j':
-        digitalWrite(ventanaHab3, HIGH);
-        Serial.println("Ventana 1 abierta");
-        break;
-      case 'k':
-        digitalWrite(ventanaHab3, LOW);
-        Serial.println("Ventana 1 Cerrada");
-        break;
-      //10
-     
-
-
-    }
-
-
-
-
+  void setup() {
+    // Configura el pin como salida
+    myservo1.attach(9);
+    myservo2.attach(10);
+    myservo3.attach(11);
+    pinMode(luzHab1, OUTPUT);
+    pinMode(luzHab2, OUTPUT);
+    pinMode(luzBano, OUTPUT);
+    pinMode(luzSala, OUTPUT);
+    pinMode(ventanaHab1, OUTPUT);
+    pinMode(ventanaHab2, OUTPUT);
+    pinMode(ventanaSala, OUTPUT);
+    pinMode(ventanaBano, OUTPUT);
+    Serial.begin(9600);
   }
 
+  void loop() {
+    if (Serial.available() > 0) {
+      option = Serial.read();
 
-}
+      switch (option) {
+        //1
+        case 'q':
+          digitalWrite(luzHab1, HIGH);
+          Serial.println("Habitacion 1 Luz encendida");
+          break;
+        case 'w':
+          digitalWrite(luzHab1, LOW);
+          Serial.println("Habitacion 1 Luz apagada");
+          break;
+        //2
+        case 'e':
+          digitalWrite(luzHab2, HIGH);
+          Serial.println("Habitacion 2 Luz encendida");
+          break;
+        case 'r':
+          digitalWrite(luzHab2, LOW);
+          Serial.println("Habitacion 2 Luz apagada");
+          break;
+        case 't':
+          //3
+          digitalWrite(luzBano, HIGH);
+          Serial.println("Habitacion 3 Luz encendida");
+          break;
+        case 'y':
+          digitalWrite(luzBano, LOW);
+          Serial.println("Habitacion 3 Luz apagada");
+          break;
+        //4
+        case 'u':
+          digitalWrite(luzSala, HIGH);
+          Serial.println("Baño Social Luz encendida");
+          break;
+        case 'i':
+          digitalWrite(luzSala, LOW);
+          Serial.println("Baño Social Luz apagada");
+          break;
+
+        //5
+        case 'o':
+          myservo1.write(90);       // mueve el servo hacia la posición 90 grados
+          delay(300);  
+          Serial.println("Bgu");
+          break;
+        case 'p':
+          myservo1.write(0);       // mueve el servo hacia la posición 90 grados
+          delay(300);  
+          Serial.println("gyuygu");
+          break;
+        
+        
+        case 'a':
+          myservo2.write(90);       // mueve el servo hacia la posición 90 grados
+          delay(300);  
+          Serial.println("Bgu");
+          break;
+        case 's':
+          myservo2.write(0);       // mueve el servo hacia la posición 90 grados
+          delay(300);  
+          Serial.println("gyuygu");
+          break;
+        
+        case 'd':
+          myservo3.write(90);       // mueve el servo hacia la posición 90 grados
+          delay(300);  
+          Serial.println("Bgu");
+          break;
+        case 'f':
+          myservo3.write(0);       // mueve el servo hacia la posición 90 grados
+          delay(300);  
+          Serial.println("gyuygu");
+          break;
+      
+          
+        //7
+        case 'g':
+          digitalWrite(ventanaHab1, HIGH);
+          Serial.println("Ventana 1 abierta");
+          break;
+        case 'h':
+          digitalWrite(ventanaHab1, LOW);
+          Serial.println("Ventana 1 Cerrada");
+          break;
+        //8
+        case 'j':
+          digitalWrite(ventanaHab2, HIGH);
+          Serial.println("Ventana 1 abierta");
+          break;
+        case 'k':
+          digitalWrite(ventanaHab2, LOW);
+          Serial.println("Ventana 1 Cerrada");
+          break;
+        //9
+        case 'l':
+          digitalWrite(ventanaSala, HIGH);
+          Serial.println("Ventana 1 abierta");
+          break;
+        case 'z':
+          digitalWrite(ventanaSala, LOW);
+          Serial.println("Ventana 1 Cerrada");
+          break;
+        //10
+        case 'x':
+          digitalWrite(ventanaBano, HIGH);
+          Serial.println("Luz baño encendida");
+          break;
+        case 'c':
+          digitalWrite(ventanaBano, LOW);
+          Serial.println("Luz baño encendida");
+          break;
+      
+
+
+      }
+    }
+  }
--- funcionArduino.py
+++ funcionArduino.py
@@ -1,18 +1,84 @@
 import serial
 
-import serial
 import time
 
 
 
 def enviarArduino(ubicaionesPuerta, ubicacionesLuz, ubicacionesVentana):
-    arduino=serial.Serial('COM3',9600)      
+    arduino=serial.Serial('COM5',9600)      
     time.sleep(2)
+#     Luces
     if ubicacionesLuz['principales']==True:
             print("Principal LuzEncendida")
-            arduino.write(b'P')
-            time.sleep(2)
+            arduino.write(b'q')
     elif ubicacionesLuz['principales']==False:
             print("Principal Apagada")
-            arduino.write(b'N')
-            time.sleep(2)
+            arduino.write(b'w')
+    if ubicacionesLuz['estudio']==True:
+            print("Estudio enecendido")
+            arduino.write(b'e')
+    elif ubicacionesLuz['estudio']==False:
+            print("Estudio Apagada")
+            arduino.write(b'r')
+    if ubicacionesLuz['baño social']==True:
+            print("Luz Baño enecendido")
+            arduino.write(b't')
+    elif ubicacionesLuz['baño social']==False:
+            print("Luz Baño  Apagada")
+            arduino.write(b'y')
+    if ubicacionesLuz['sala']==True:
+            print("Luz sala enecendido")
+            arduino.write(b'u')
+    elif ubicacionesLuz['sala social']==False:
+            print("Luz sala  Apagada")
+            arduino.write(b'i')
+
+# PUERTAS
+    if ubicaionesPuerta['principales']==True:
+            print("Puerta principales abierta")
+            arduino.write(b'o')
+    elif ubicaionesPuerta['principales']==False:
+            print("Puerta cerrada  Apagada")
+            arduino.write(b'p')
+    if ubicaionesPuerta['estudio']==True:
+            print("Puerta estudio abierta")
+            arduino.write(b'a')
+    elif ubicaionesPuerta['estudio']==False:
+            print("Puerta estudio cerrada ")
+            arduino.write(b's')
+    if ubicaionesPuerta['baño social']==True:
+            print("Puerta baño social abierta")
+            arduino.write(b'd')
+    elif ubicaionesPuerta['baño social']==False:
+            print("Puerta baño social cerrada ")
+            arduino.write(b'f')
+
+    ## VENTANAS 
+    if ubicacionesVentana['principales']==True:
+            print("Ventanta principales abierta")
+            arduino.write(b'g')
+    elif ubicacionesVentana['principales']==False:
+            print("Ventanta principale  Cerrada")
+            arduino.write(b'h')
+
+    if ubicacionesVentana['estudio']==True:
+            print("Ventanta estudio abierta")
+            arduino.write(b'j')
+    elif ubicacionesVentana['estudio']==False:
+            print("Ventanta estudio  Cerrada")
+            arduino.write(b'k')
+
+
+    if ubicacionesVentana['sala']==True:
+            print("Ventanta sala abierta")
+            arduino.write(b'l')
+    elif ubicacionesVentana['sala']==False:
+            print("Ventanta sala  Cerrada")
+            arduino.write(b'z')
+    
+    if ubicacionesVentana['baño social']==True:
+            print("Ventanta baño social abierta")
+            arduino.write(b'x')
+    elif ubicacionesVentana['baño social']==False:
+            print("Ventanta baño social  Cerrada")
+            arduino.write(b'c')
